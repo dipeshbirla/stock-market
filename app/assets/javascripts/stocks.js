4c8fa0594c7bd01fa6388e19bb7a93f6fa37baa1
dipeshbirla <dipeshbirla57@gmail.com>
2021-02-28 00:36:22 +0530

    Refactored the code.

---
+++ app/assets/javascripts/stocks.js
@@ -0,0 +1,5 @@
+$(document).ready(function(){
+  $('#stock-lookup-form').submit(function(){
+    $('.look-up-a-stock').prop('disabled', true);
+  });
+});
--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,15 +1,20 @@
+# frozen_string_literal: true
+
+# StocksController
 class StocksController < ApplicationController
-
-    def search
-      if params[:stock]
-          @stock = Stock.find_by_ticker(params[:stock])
-          @stock ||=Stock.new_from_lookup(params[:stock])
-
+  def search
+    if params[:stock]
+      @stock = Stock.find_by_ticker(params[:stock])
+      begin
+        @stock ||= Stock.new_from_lookup(params[:stock])
+      rescue StandardError => e
+        @error = true
+        @stock = []
+        flash.now[:notice] = 'There is no stock of this symbol'
       end
-
-      if @stock
-        # render partial: 'lookup'
-      else
-      end  	
     end
+    respond_to do |format|
+      format.js { render :search }
+    end
+  end
 end
