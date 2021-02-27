b00edc0725330cf11f6146af3039f535ca9d26f7
dipeshbirla <dipeshbirla57@gmail.com>
2021-02-27 16:40:22 +0530

    added x-ray gem. - Fixed rubocops.

--- app/controllers/user_stocks_controller.rb
+++ app/controllers/user_stocks_controller.rb
@@ -1,5 +1,8 @@
+# frozen_string_literal: true
+
+# UserStocksController
 class UserStocksController < ApplicationController
-  before_action :set_user_stock, only: [:show, :edit, :update]
+  before_action :set_user_stock, only: %i[show edit update]
   # GET /user_stocks
   # GET /user_stocks.json
   def index
@@ -28,22 +31,25 @@ class UserStocksController < ApplicationController
     @user_stock = UserStock.new(stock_id: params[:stock_id], user: current_user)
     else
       stock = Stock.find_by_ticker(params[:stock_ticker])
-        if stock
+      if stock
+        @user_stock = UserStock.new(user: current_user, stock_id: stock.id)
+      else
+        stock = Stock.new_from_lookup(params[:stock_ticker])
+        if stock.save
           @user_stock = UserStock.new(user: current_user, stock_id: stock.id)
         else
-          stock = Stock.new_from_lookup(params[:stock_ticker])
-          if stock.save
-            @user_stock = UserStock.new(user: current_user, stock_id: stock.id)
-          else
-            @user_stock = nil
-            flash[:error] = "Stock is not available"  
-          end
+          @user_stock = nil
+          flash[:error] = 'Stock is not available'
         end
+      end
     end
 
     respond_to do |format|
       if @user_stock.save
-        format.html { redirect_to my_portfolio_path, notice: "Stock #{@user_stock.stock.ticker} stock was successfully added"}
+        format.html do
+          msg = "Stock #{@user_stock.stock.ticker} stock was successfully added"
+          redirect_to my_portfolio_path, notice: msg
+        end
         format.json { render :show, status: :created, location: @user_stock }
       else
         format.html { render :new }
@@ -57,7 +63,10 @@ class UserStocksController < ApplicationController
   def update
     respond_to do |format|
       if @user_stock.update(user_stock_params)
-        format.html { redirect_to @user_stock, notice: 'User stock was successfully updated.' }
+        format.html do
+          redirect_to @user_stock,
+                      notice: 'User stock was successfully updated.'
+        end
         format.json { render :show, status: :ok, location: @user_stock }
       else
         format.html { render :edit }
@@ -69,22 +78,29 @@ class UserStocksController < ApplicationController
   # DELETE /user_stocks/1
   # DELETE /user_stocks/1.json
   def destroy
-    @user_stock = Stock.find(params[:id]).user_stocks.where(stock_id:params[:id],user_id:current_user.id).first
+    @user_stock = Stock.find(params[:id]).user_stocks
+                       .where(stock_id: params[:id], user_id: current_user.id)
+                       .first
     @user_stock.destroy
     respond_to do |format|
-      format.html { redirect_to my_portfolio_path, notice: 'Stock was Successfully removed from my_portfolio' }
+      format.html do
+        redirect_to my_portfolio_path,
+                    notice: 'Stock was Successfully removed from my_portfolio'
+      end
       format.json { head :no_content }
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_user_stock
-      @user_stock = UserStock.find(params[:id])
-    end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def user_stock_params
-      params.require(:user_stock).permit(:user_id, :stock_id)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_user_stock
+    @user_stock = UserStock.find(params[:id])
+  end
+
+  # Never trust parameters from the scary internet, only allow the white list
+  # through.
+  def user_stock_params
+    params.require(:user_stock).permit(:user_id, :stock_id)
+  end
 end
